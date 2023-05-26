d3729abecdb03f071977bbc75079c1c78bca2ebd
yuliiaaaaa <105826111+yuliiaaaaa@users.noreply.github.com>
2023-05-26 13:17:47 +0300

    Delete src/modules directory

--- src/modules/posts/post.entity.ts
+++
@@ -1,5 +0,0 @@
-export interface PostEntity {
-  id: string;
-  title: string;
-  content: string;
-}
--- src/modules/posts/serializers/post.serializer.ts
+++
@@ -1,9 +0,0 @@
-import { Expose } from "class-transformer";
-
-export class PostSerializer {
-  @Expose()
-  id = "";
-
-  @Expose()
-  title = "";
-}
